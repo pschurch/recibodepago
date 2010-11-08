6f3a14d88fba33d0e7711502a499d2f288f0370b
Charles Haramoto <charles.haramoto@gmail.com>
2010-11-08 17:52:43 -0300

    Commit inicial

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0'
+gem 'mysql2'
+
+group :development do
+  gem 'rspec-rails','2.0.1'
+  gem 'annotate-models', '1.0.4'
+end
+
+group :test do
+  gem 'rspec', '2.0.1'
+  gem 'webrat', '0.7.1'
+  gem 'annotate-models', '1.0.4'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recibodepago::Application.config.secret_token = 'bea3c29b564546d4dcc080d3a11f13b206021304d7fa8f128ef39c5a58135cb923ec8dcefd75cc02a28f1939c4bcfee54049ce2c54a4f4d3e090465331c0ea23'
