cc0cd0fde45ff65bdbdc217ef356fd7f7c2f5fed
Charles Haramoto <charles.haramoto@gmail.com>
2010-11-25 14:08:08 -0300

    ok

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,31 @@
 module ApplicationHelper
  #Title en el browser para las páginas
  def titulo_pagina
-  titulo_base = "Recibo de Pago"
-  if @perfil_name.nil?
-    titulo_base
-  else
-    "#{titulo_base} | #{@perfil_name}"
-  end
+   titulo_base = "Recibo de Pago"
+   if @perfil_name.nil?
+     titulo_base
+   else
+     "#{titulo_base} | #{@perfil_name}"
+   end
  end
 
-#Titulo principal de cada página para el bloque titulo
+ #Titulo principal de cada página para el bloque titulo
  def titulo_principal
-  titulo_base = "Pagina Principal"
-  if @perfil_name.nil?
-    titulo_base
-  else
-    if @titulo.nil?
-      "#{@perfil_name} - #{titulo_base}"
-    else
-      "#{@perfil_name} - #{@titulo} "
-    end
-  end
+   if @titulo_unico.nil?
+     titulo_base = "Pagina Principal"
+   else
+     titulo_base = @titulo_unico
+   end
+   if @perfil_name.nil?
+     titulo_base
+   else
+     if @titulo.nil?
+       "#{@perfil_name} - #{titulo_base}"
+     else
+       "#{@perfil_name} - #{@titulo} "
+     end
+   end
  end
+
 end
 
