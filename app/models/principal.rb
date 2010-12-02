7fda097c350e9e0a64293bc466e22e210c448e06
Charles Haramoto <charles.haramoto@gmail.com>
2010-12-02 17:51:55 -0300

    ok

--- app/models/principal.rb
+++ app/models/principal.rb
@@ -17,17 +17,16 @@
 #
 
 class Principal < ActiveRecord::Base
+  has_many :payment_policies
+  has_many :tickets
+
   validates_presence_of :name, :message => "(Nombre) : debe ingresar un valor en este campo."
-
   validates_presence_of :social_reason, :message => "(Razon Social) : debe ingresar un valor en este campo."
-
    validates_format_of :rut, :with => /\A[+-]?\d+\Z/, :message => "(Rut) : debe ingresar solamente digitos, sin puntos.", :allow_blank => true
-
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i, :message => ": la direccion de correo no es valida.", :allow_blank => true 
-   
   validates_inclusion_of :digit, :in => %w(0 1 2 3 4 5 6 7 8 9 k K),  :message => "(Digito verificador) no es un valor valido.", :allow_blank => true
 
-  has_many :payment_policies
+
 end
 
 
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,9 @@
 class Ticket < ActiveRecord::Base
+  belongs_to :principal
+  belongs_to :product
+  belongs_to :user
+  belongs_to :collection_type
+
   validates_presence_of :rut, :message => "(Rut) : debe ingresar un valor en este campo."
   validates_presence_of :name, :message => "(Nombre Deudor) : debe ingresar un valor en este campo."
   validates_presence_of :cas_id, :message => "(Identificador) : debe ingresar un valor en este campo."
