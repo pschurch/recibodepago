0ecf25e7a4e9b765be9dd1ac4813b503d99cd664
Charles Haramoto <charles.haramoto@gmail.com>
2010-12-22 09:58:03 -0300

    ok

--- app/models/principal.rb
+++ app/models/principal.rb
@@ -23,9 +23,42 @@ class Principal < ActiveRecord::Base
 
   validates_presence_of :name, :message => "(Nombre) : debe ingresar un valor en este campo."
   validates_presence_of :social_reason, :message => "(Razon Social) : debe ingresar un valor en este campo."
-   validates_format_of :rut, :with => /\A[+-]?\d+\Z/, :message => "(Rut) : debe ingresar solamente digitos, sin puntos.", :allow_blank => true
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i, :message => ": la direccion de correo no es valida.", :allow_blank => true 
+  validate :valida_rut
+  validates_format_of :rut, :with => /\A[+-]?\d+\Z/, :message => " : debe ingresar solamente digitos y sin puntos ni guiones.", :allow_blank => true
   validates_inclusion_of :digit, :in => %w(0 1 2 3 4 5 6 7 8 9 k K),  :message => "(Digito verificador) no es un valor valido.", :allow_blank => true
+
+  def valida_rut
+    if !rut.blank? 
+      largo = rut.length
+      if (largo<7 or largo>9)
+        errors.add(:rut, " : el valor ingresado no es valido.")
+      elsif digit.blank?
+        errors.add(:digit, "(Rut) : debe ingresar el digito verificador.")
+      else
+        sum = 0
+        for i in 1..largo	
+          if i<7
+            val = rut[largo-i].to_i * (i+1)
+          else
+            val = rut[largo-i].to_i * (i-5)
+          end
+          sum = sum + val
+        end
+        res = 11 - (sum%11)
+        if res==11
+          digito = "0"
+        elsif res==10
+          digito = "K"
+        else
+          digito = res.to_s
+        end
+        if digit!=digito
+          errors.add(:rut, " : el valor ingresado no es valido.")
+        end
+      end
+    end
+  end
 
 
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,9 +4,45 @@ class Ticket < ActiveRecord::Base
   belongs_to :user
   belongs_to :collection_type
 
-  validates_presence_of :rut, :message => "(Rut) : debe ingresar un valor en este campo."
   validates_presence_of :name, :message => "(Nombre Deudor) : debe ingresar un valor en este campo."
   validates_presence_of :cas_id, :message => "(Identificador) : debe ingresar un valor en este campo."
 
+  validate :valida_rut
+  validates_format_of :rut, :with => /\A[+-]?\d+\Z/, :message => " : debe ingresar solamente digitos y sin puntos ni guiones.", :allow_blank => true
+  validates_inclusion_of :digit, :in => %w(0 1 2 3 4 5 6 7 8 9 k K),  :message => "(Digito verificador) no es un valor valido.", :allow_blank => true
+
+  def valida_rut
+    if rut.blank?
+      errors.add(:rut, " : debe ingresar un valor en este campo.")
+    else !rut.blank? 
+      largo = rut.length
+      if (largo<7 or largo>9)
+        errors.add(:rut, " : el valor ingresado no es valido.")
+      elsif digit.blank?
+        errors.add(:digit, "(Rut) : debe ingresar el digito verificador.")
+      else
+        sum = 0
+        for i in 1..largo	
+          if i<7
+            val = rut[largo-i].to_i * (i+1)
+          else
+            val = rut[largo-i].to_i * (i-5)
+          end
+          sum = sum + val
+        end
+        res = 11 - (sum%11)
+        if res==11
+          digito = "0"
+        elsif res==10
+          digito = "K"
+        else
+          digito = res.to_s
+        end
+        if digit!=digito
+          errors.add(:rut, " : el valor ingresado no es valido.")
+        end
+      end
+    end
+  end
 
 end
