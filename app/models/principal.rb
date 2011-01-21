e7a902991cc6e0caa6ad782aa0b1a4dc68469168
Paola Schurch <paola.schurch@gmail.com>
2011-01-20 22:07:38 -0300

    ok

--- app/models/principal.rb
+++ app/models/principal.rb
@@ -26,7 +26,7 @@ class Principal < ActiveRecord::Base
   validates_presence_of :name, :message => "(Nombre) : debe ingresar un valor en este campo."
   validates_presence_of :social_reason, :message => "(Razon Social) : debe ingresar un valor en este campo."
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i, :message => ": la direccion de correo no es valida.", :allow_blank => true 
-  validate :valida_rut
+#  validate :valida_rut
   validates_format_of :rut, :with => /\A[+-]?\d+\Z/, :message => " : debe ingresar solamente digitos y sin puntos ni guiones.", :allow_blank => true
   validates_inclusion_of :digit, :in => %w(0 1 2 3 4 5 6 7 8 9 k K),  :message => "(Digito verificador) no es un valor valido.", :allow_blank => true
 
